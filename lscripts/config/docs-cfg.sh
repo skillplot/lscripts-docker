ac062741da5cc93ab9b9d62466e699128ae5368a
mangalbhaskar <skillplot@gmail.com>
2021-07-19 04:57:09 +0530

    lscripts config override in gitignore and missed out configs are added

---
+++ lscripts/config/docs-cfg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Copyright (c) 2021 mangalbhaskar. All Rights Reserved.
+##__author__ = 'mangalbhaskar'
+###----------------------------------------------------------
+## configuration for docs
+###----------------------------------------------------------
